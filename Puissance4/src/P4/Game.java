3d7562d464bee030da30051ef14ed260e5c63fab
Sevaeb <seb-1994@hotmail.fr>
2013-10-29 01:26:48 +0100

    Added method "aligne"

--- Puissance4/src/P4/Game.java
+++ Puissance4/src/P4/Game.java
@@ -11,12 +11,16 @@ public class Game extends JPanel {
 	private static final long serialVersionUID = 1L;
 	public int[] numColonne;
 	public char[][] grille;
+	public int[] nombreAligne;
 	public static final int TAILLE_JETON = 75;
-	public static final int NOMBRE_TOUR = 42;
+	private static final int NOMBRE_TOUR = 42;
+	private static final char JAUNE = 'J';
+	private static final char ROUGE = 'R';
+	private static final char VIDE = 'O';
 	private Scanner colonne;
-	private Scanner couleurJeton;
+	//private Scanner couleurJeton;
 	
-	int i,j,c;
+	int i,j,c, numeroTour;
 	
 	
 	public Game(){
@@ -33,11 +37,12 @@ public class Game extends JPanel {
 		
 		for(i=0;i<7;i++){
 			for(j=0;j<6;j++){
-				this.grille[i][j] = 'O';
+				this.grille[i][j] = VIDE;
 			}
 		}
 	}
 	public void affiche(){
+		System.out.println();
 		for(c=0;c<7;c++){
 			System.out.print(this.numColonne[c]);
 		}
@@ -62,15 +67,15 @@ public class Game extends JPanel {
     	for(i=0;i<7;i++){
     		for(j=0;j<6;j++){
     			switch (this.grille[i][j]){
-    			case 'O' :
+    			case VIDE :
     				g.setColor(Color.blue);
     		    	g.fillOval(i*TAILLE_JETON, j*TAILLE_JETON +30, TAILLE_JETON, TAILLE_JETON);
     		    	break;
-    			case 'R' :
+    			case ROUGE :
     				g.setColor(Color.red);
     		    	g.fillOval(i*TAILLE_JETON, j*TAILLE_JETON +30, TAILLE_JETON, TAILLE_JETON);
     		    	break;
-    			case 'J' :
+    			case JAUNE :
     				g.setColor(Color.yellow);
     		    	g.fillOval(i*TAILLE_JETON, j*TAILLE_JETON +30, TAILLE_JETON, TAILLE_JETON);
     		    	break;
@@ -79,7 +84,16 @@ public class Game extends JPanel {
     	}
     }
 	public void jouer(){
+		char couleurJouee;
 		int ligneJeu = 0;
+		if ((numeroTour %2) == 0){
+			couleurJouee = ROUGE;
+			System.out.println("Au tour de la couleur ROUGE");
+		}
+		else{
+			couleurJouee = JAUNE;
+			System.out.println("Au tour de la couleur JAUNE");
+		}
 		colonne = new Scanner(System.in);
 		System.out.println("Veuillez saisir le numéro de colonne dans laquelle vous voulez jouer :");
 		int num = colonne.nextInt();
@@ -88,16 +102,16 @@ public class Game extends JPanel {
 			System.out.println("Veuillez redonner un numéro de colonne entre 1 et 7 :");
 			num = colonne.nextInt();
 		}
-		couleurJeton = new Scanner(System.in);
+		/*couleurJeton = new Scanner(System.in);
 		System.out.println("Veuillez indiquer la couleur de votre jeton (J pour jaune, R pour rouge) :");
 		char couleurJouee = couleurJeton.nextLine().charAt(0);
-		while(couleurJouee != 'J' && couleurJouee != 'R'){
+		while(couleurJouee != JAUNE && couleurJouee != ROUGE){
 			System.out.println("La couleur donnée n'est pas bonne.");
 			System.out.println("Veuillez indiquer la couleur de votre jeton (J pour jaune, R pour rouge) :");
 			couleurJouee = couleurJeton.nextLine().charAt(0);
-		}
-		if(this.grille[num-1][0] == 'O'){
-			while(ligneJeu<6 && this.grille[num-1][ligneJeu] == 'O'){
+		}*/
+		if(this.grille[num-1][0] == VIDE){
+			while(ligneJeu<6 && this.grille[num-1][ligneJeu] == VIDE){
 				ligneJeu++;
 			}
 			this.grille[num-1][ligneJeu-1] = couleurJouee;
@@ -109,30 +123,107 @@ public class Game extends JPanel {
 		aligne(num-1,ligneJeu-1);
 	}
 	public void aligne(int colonne, int ligne){
-		int nombreAligne = 0;
+		this.nombreAligne = new int[4];
+		this.nombreAligne[0]=1; // nombreAligne sur une colonne
+		this.nombreAligne[1]=1; // nombreAligne sur une ligne
+		this.nombreAligne[2]=1; // nombreAligne sur la diagonale principale
+		this.nombreAligne[3]=1; // nombreAligne sur la seconde diagonale
 		char couleur;
 		couleur = this.grille[colonne][ligne];
-		if(this.grille[colonne-1][ligne] == couleur){
-			nombreAligne = nombreAligne + 1;
+		// Test l'alignement sur une colonne
+		if((ligne+1)<6 && this.grille[colonne][ligne+1] == couleur){
+			nombreAligne[0] = nombreAligne[0] + 1;
+			if((ligne+2)<6 && this.grille[colonne][ligne+2] == couleur){
+				nombreAligne[0] = nombreAligne[0] + 1;
+				if((ligne+3)<6 && this.grille[colonne][ligne+3] == couleur){
+					nombreAligne[0] = nombreAligne[0] + 1;
+				}
+			}
+		}
+		// Test l'alignement sur une ligne 
+		if((colonne-1)>=0 && this.grille[colonne-1][ligne] == couleur){
+			nombreAligne[1] = nombreAligne[1] + 1;
+			if((colonne-2)>=0 && this.grille[colonne-2][ligne] == couleur){
+				nombreAligne[1] = nombreAligne[1] + 1;
+				if((colonne-3)>=0 && this.grille[colonne-3][ligne] == couleur){
+					nombreAligne[1] = nombreAligne[1] + 1;
+				}
+			}
+		}
+		if((colonne+1)<7 && this.grille[colonne+1][ligne] == couleur){
+			nombreAligne[1] = nombreAligne[1] + 1;
+			if((colonne+2)<7 && this.grille[colonne+2][ligne] == couleur){
+				nombreAligne[1] = nombreAligne[1] + 1;
+				if((colonne+3)<7 && this.grille[colonne+3][ligne] == couleur){
+					nombreAligne[1] = nombreAligne[1] + 1;
+				}
+			}
+		}
+		// Test l'alignement sur la diagonale principale
+		if((colonne+1)<7 && (ligne+1)<6 && this.grille[colonne+1][ligne+1] == couleur){
+			nombreAligne[2] = nombreAligne[2] + 1;
+			if((colonne+2)<7 && (ligne+2)<6 && this.grille[colonne+2][ligne+2] == couleur){
+				nombreAligne[2] = nombreAligne[2] + 1;
+				if((colonne+3)<7 && (ligne+3)<6 && this.grille[colonne+3][ligne+3] == couleur){
+					nombreAligne[2] = nombreAligne[2] + 1;
+				}
+			}
+		}
+		if((colonne-1)>=0 && (ligne-1)>=0 && this.grille[colonne-1][ligne-1] == couleur){
+			nombreAligne[2] = nombreAligne[2] + 1;
+			if((colonne-2)>=0 && (ligne-2)>=0 && this.grille[colonne-2][ligne-2] == couleur){
+				nombreAligne[2] = nombreAligne[2] + 1;
+				if((colonne-3)>=0 && (ligne-3)>=0 && this.grille[colonne-3][ligne-3] == couleur){
+					nombreAligne[2] = nombreAligne[2] + 1;
+				}
+			}
+		}
+		// Test l'alignement sur la seconde diagonale
+		if((colonne-1)>=0 && (ligne+1)<6 && this.grille[colonne-1][ligne+1] == couleur){
+			nombreAligne[3] = nombreAligne[3] + 1;
+			if((colonne-2)>=0 && (ligne+2)<6 && this.grille[colonne-2][ligne+2] == couleur){
+				nombreAligne[3] = nombreAligne[3] + 1;
+				if((colonne-3)>=0 && (ligne+3)<6 && this.grille[colonne-3][ligne+3] == couleur){
+					nombreAligne[3] = nombreAligne[3] + 1;
+				}
+			}
+		}
+		if((colonne+1)<7 && (ligne-1)>=0 && this.grille[colonne+1][ligne-1] == couleur){
+			nombreAligne[3] = nombreAligne[3] + 1;
+			if((colonne+2)<7 && (ligne-2)>=0 && this.grille[colonne+2][ligne-2] == couleur){
+				nombreAligne[3] = nombreAligne[3] + 1;
+				if((colonne+3)<7 && (ligne-3)>=0 && this.grille[colonne+3][ligne-3] == couleur){
+					nombreAligne[3] = nombreAligne[3] + 1;
+				}
+			}
 		}
 		
+		// Affichage du gagnant
+		if(nombreAligne[0] == 4 || nombreAligne[1] == 4 || nombreAligne[2] == 4 || nombreAligne[3] == 4){
+			if(couleur == ROUGE){
+				System.out.println("La couleur ROUGE l'emporte.");
+				numeroTour = NOMBRE_TOUR;
+			}
+			else{
+				System.out.println("La couleur JAUNE l'emporte.");
+				numeroTour = NOMBRE_TOUR;
+			}
+		}		
 	}
 	public void run()
 	{
+		numeroTour = 0;
 		affiche();
-		grille[5][5]='R';
-		affiche();
-		repaint();
-		while( this.grille[0][0] == 'O' || this.grille[1][0] == 'O' || this.grille[2][0] == 'O' || this.grille[3][0] == 'O' 
-				|| this.grille[4][0] == 'O' || this.grille[5][0] == 'O' || this.grille[6][0] == 'O'){
+		while( numeroTour <= NOMBRE_TOUR){
 			try{
-				Thread.sleep(2000);
+				Thread.sleep(1000);
 			} catch (InterruptedException e){
 				e.printStackTrace();
-		}
+			}
 		jouer();
 		affiche();
 		repaint();
+		numeroTour++;
 		}
 		
 	}
